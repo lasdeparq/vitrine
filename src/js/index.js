85d1b5e2af4ceea01904930b95b1c60ff05ec3b0
André Lacerda da Silva <lasdeparq@gmail.com>
2017-11-07 23:30:51 -0200

    versao final sem ajustes

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@ class Item {
             this[attr] = item[attr];
         }
 
+        //TODO Extract to methods
         /**image */
         let img = document.createElement("img");
         img.setAttribute("src", this.imageName);
@@ -47,16 +48,98 @@ class Item {
 
         /**item url and container element */
         this.itemElem = document.createElement("a");
+        this.itemElem.classList.add("hidden");
         this.itemElem.setAttribute("href", this.detailUrl);
         this.itemElem.setAttribute("target", "_blank");
         this.itemElem.appendChild(divItem);
 
     }
 
-    getItemElement() {
-        return this.itemElem;
+    show() {
+        this.itemElem.classList.remove("hidden");
     }
+
+    hide() {
+        this.itemElem.classList.add("hidden");
+    }
+
 }
+
+function X(response) {
+    let referenceItem = new Item(response.data.reference.item);
+    let recommended = response.data.recommendation;
+    init(referenceItem, recommended);
+}
+
+//TODO Create a namespace
+let pages;
+let pageSelected = 0;
+let pageSize = 3;
+const items = [];
+
+const disable = function () {
+    this.querySelector("i").classList.add("disabled");
+};
+const enable = function () {
+    this.querySelector("i").classList.remove("disabled");
+};
+
+const prev = document.querySelector(".items__recommended__previous");
+prev.onclick = function () {
+    if (pageSelected !== 0) {
+        for (let i = 0; i < items.length; i++) {
+            items[i].hide();
+        }
+
+        pageSelected--;
+        items.show(pageSelected);
+        enableDisableNextPrevious();
+    }
+};
+prev.disable = disable;
+prev.enable = enable;
+
+const next = document.querySelector(".items__recommended__next");
+next.onclick = function () {
+    if (pageSelected !== pages - 1) {
+        for (let i = 0; i < items.length; i++) {
+            items[i].hide();
+        }
+
+        pageSelected++;
+        items.show(pageSelected);
+        enableDisableNextPrevious();
+    }
+};
+next.disable = disable;
+next.enable = enable;
+
+function init(referenceItem, recommendedItems) {
+    document.querySelector(".items__reference").appendChild(referenceItem.itemElem);
+
+    pages = Math.ceil(recommendedItems.length / pageSize);
+    for (let i = 0; i < recommendedItems.length; i++) {
+        let newItem = new Item(recommendedItems[i]);
+        items.push(newItem);
+        document.querySelector(".items__recommended").appendChild(newItem.itemElem);
+
+    }
+
+    referenceItem.show();
+    items.show(0);
+
+    spinner.hide();
+    enableDisableNextPrevious();
+}
+
+
+items.show = function (index) {
+    for (let i = 0; i < pageSize; i++) {
+        if (typeof items[i + index * pageSize] !== "undefined") {
+            items[i + index * pageSize].show();
+        }
+    }
+};
 
 const spinner = document.querySelector(".loading");
 spinner.hide = function () {
@@ -66,12 +149,23 @@ spinner.show = function () {
     spinner.classList.remove("hidden");
 };
 
-function X(response) {
+function enableDisableNextPrevious() {
+    if (pages > 1) {
+        if (pageSelected === 0) {
+            prev.disable();
+        } else {
+            prev.enable();
+            if (pageSelected === pages - 1) {
+                next.disable();
+            } else {
+                next.enable();
+            }
+        }
+    } else {
+        prev.disable();
+        next.disable();
+    }
 
-    let newItem = new Item(response.data.reference.item);
-    document.querySelector(".items__reference").appendChild(newItem.getItemElement());
-    console.log(newItem);
-    spinner.hide();
 }
 
 
